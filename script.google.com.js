bf76769e763ae7fb386a88c5c6d444a088b3c54c
Thái <62926491+nhthai173@users.noreply.github.com>
2022-11-19 21:34:48 +0700

    Update script.google.com.js

--- script.google.com.js
+++ script.google.com.js
@@ -1,1 +1,10 @@
-document.getElementById('warning').remove()
+var url = window.location.href
+
+// for script.google.com
+if (url.includes('script.google.com') && url.includes('/exec')) {
+    function removeWarning() {
+        const w = document.getElementById('warning')
+        if (w) w.innerHTML = ""
+    }
+    setInterval(removeWarning, 1000);
+}
