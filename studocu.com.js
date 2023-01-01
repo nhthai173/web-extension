8a7e871e8b4885cbb5b46439b2a2a9b95020abe8
Thái <62926491+nhthai173@users.noreply.github.com>
2023-01-01 09:39:35 +0700

    Update to match with new global css

--- studocu.com.js
+++ studocu.com.js
@@ -32,7 +32,7 @@ function onLocationChange() {
 }
 
 function onPageBgImgChange(mode) {
-    if (!mode) mode = 'auto';
+    if (!mode) mode = currentPageBgMode;
     currentPageBgMode = mode;
     document.querySelectorAll('.pageBgImg').forEach(btn => btn.classList.remove('active'))
     document.querySelector(`.pageBgImg[data-page-bg="${mode}"]`).classList.add('active');
@@ -52,16 +52,19 @@ function onPageBgImgChange(mode) {
 function initModal() {
     const m = document.querySelector('.nht_modal-content');
     if (!m) return;
+    if (isInit) return showModal();
     m.innerHTML = `
-        <div class="d-flex-center mb-8">
+        <div class="d-flex-center mb-8-px">
             <h3 class="fw=500">Page Background</h3>
         </div>
         <div class="d-flex-center">
-            <button class="ms-8 pageBgImg btn-page-bg" data-page-bg="auto">Auto</button>
-            <button class="ms-15 pageBgImg btn-page-bg" data-page-bg="hide">Remove</button>
-            <button class="ms-15 me-8 pageBgImg btn-page-bg" data-page-bg="show">Show</button>
+            <button class="ms-8-px pageBgImg btn-page-bg" data-page-bg="auto">Auto</button>
+            <button class="ms-15-px pageBgImg btn-page-bg" data-page-bg="hide">Remove</button>
+            <button class="ms-15-px me-8-px pageBgImg btn-page-bg" data-page-bg="show">Show</button>
         </div>`;
-    document.querySelector('nht').classList.remove('d-none-important');
+    showModal();
+    onPageBgImgChange();
+    isInit = true;
 }
 
 
@@ -69,22 +72,25 @@ function hideModal() {
     document.querySelector('nht').classList.add('d-none-important');
 }
 
+function showModal() {
+    document.querySelector('nht').classList.remove('d-none-important');
+}
+
 
 var currentPageBgMode = 'auto'
+var isInit = false;
 run()
 function run() {
     document.dispatchEvent(new CustomEvent('nhtcss.matchWithDarkReader'));
     stuInit();
     onLocationChange();
-    onPageBgImgChange();
+    setTimeout(onLocationChange, 3000);
+    setInterval(onLocationChange, 20000);
     document.addEventListener(new CustomEvent('location.change'), onLocationChange);
+    document.querySelectorAll('.pageBgImg').forEach(btn => {
+        btn.addEventListener('click', e => {
+            const mode = e.target.getAttribute('data-page-bg');
+            onPageBgImgChange(mode);
+        })
+    });
 }
-
-document.querySelectorAll('.pageBgImg').forEach(btn => {
-    btn.addEventListener('click', e => {
-        const mode = e.target.getAttribute('data-page-bg');
-        onPageBgImgChange(mode);
-    })
-})
-
-/* document.addEventListener('nhtcss.modal.show', e => {}); */
