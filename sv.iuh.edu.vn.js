61895e09f12eae41a287404bb204a447797c69bd
Thái <62926491+nhthai173@users.noreply.github.com>
2023-01-08 12:01:14 +0700

    Update sv.iuh.edu.vn.js

--- sv.iuh.edu.vn.js
+++ sv.iuh.edu.vn.js
@@ -1,15 +1,20 @@
 const autoCompleteForm = () => {
+    let anySuccess = false
     try {
         document.querySelectorAll('.group_cauhoi .group-cautraloi').forEach(div => {
             const $inputs = div.querySelectorAll('input')
             if ($inputs && $inputs.length) {
                 $inputs[ 3 ].checked = true
+                anySuccess = true
             }
         })
         document.querySelectorAll('.input-ykien').forEach(yk => {
             yk.value = 'Không có'
+            anySuccess = true
         })
-        document.querySelector('input[type="submit"]').dispatchEvent(new PointerEvent('click', { bubbles: true }))
+        if (anySuccess) {
+            document.querySelector('input[type="submit"]').dispatchEvent(new PointerEvent('click', { bubbles: true }))
+        }
     } catch (e) { }
 }
 
